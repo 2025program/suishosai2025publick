cc5a9871df1d184920c9f86b3cf91c13c93365d4
2025program <suiranprogram@gmail.com>
2025-06-13 07:13:50 +0900

    eventpage DevOnly

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -1,10 +1,13 @@
 import React, { Suspense } from 'react';
 import ClientEventPage from '@/components/event/event';
+import { DevOnly } from '@/components/DevOnly';
 
 const EventPage: React.FC = () => {
     return (
         <Suspense fallback={<div>読み込み中...</div>}>
-            <ClientEventPage />
+            <DevOnly>
+                <ClientEventPage />
+            </DevOnly>
         </Suspense>
     );
 };
