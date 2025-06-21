49edb098be728d9fb5229dca1a26f1eb064cf88e
2025program <suiranprogram@gmail.com>
2025-06-21 11:16:47 +0900

    フッター追加

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,11 +2,13 @@
 
 import { Suspense } from "react";
 import Three from "./Three";
+import Footer from "@/components/footer/footer";
 
 export default function ThreePage() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Three />
+            <Footer></Footer>
         </Suspense>
     );
 }
